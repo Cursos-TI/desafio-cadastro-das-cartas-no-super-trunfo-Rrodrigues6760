938face02ce9f6ea3a325e83c36ba03bbe081125
Rrodrigues6760 <rrodrigues6760@gmail.com>
2025-02-02 23:02:35 +0000

    ajustado para imprimir

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,14 +2,14 @@
 
 int main (){
 char Nome [50];
-float População, Área, PIB;
+float Populacao, Área, PIB;
 int turísticos;
 
 printf("Digite o nome da Cidade: \n");
 scanf ("%s", &Nome);
 
 printf("Digite a População: \n");
-scanf("%f", &População);
+scanf("%f", &Populacao);
 
 printf("Digite a Área: \n");
 scanf("%f", &Área);
@@ -17,9 +17,14 @@ scanf("%f", &Área);
 printf("Digite o PIB: \n");
 scanf("%f", &PIB);
 
-printf("Digite o número de pontos turisticos: \n");
+printf("Digite o número de pontos turísticos: \n");
 scanf("%s", &turísticos);
 
+printf("Nome da Cidade: %s \n", Nome);
+printf("Populacao: %f \n", Populacao);
+Printf("Área: %f \n", Área);
+printf("PIB: %f \n",PIB);
+printf("Número de pontos turiísticos: %f \n" ,turísticos);
 
 return 0;
 
