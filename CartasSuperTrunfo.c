acd8291a6bf8eaadae4960e3946cb0dbf256b2e8
Rrodrigues6760 <rrodrigues6760@gmail.com>
2025-02-22 18:18:56 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,30 +1,97 @@
 #include <stdio.h>
 
-int main (){
-char Nome [50];
-float Populacao, Área, PIB;
-int turísticos;
 
-printf("Digite o nome da Cidade: \n");
-scanf ("%s", &Nome);
+int main() { printf("Desafio SuperTrunfo\n");
+printf("-----+ BEM VINDO AO JOGO SUPER TRUNFO +-----\n");
+printf("------+ CARTA DE JOGADOR 1: MARCOS +------ \n");
+    char nome_do_estado[20] = "Minas Gerais"  ;
+    char codigo_da_carta[6] = "A01" ;
+    char nome_da_cidade[25] = "Belo horizonte"  ;
+    int populacao = 889655258 ;
+    float area_em_km = 230.6 ;
+    float pib = 18.3 ;
+    int numero_de_pontos_turisticos = 18 ;
 
-printf("Digite a População: \n");
-scanf("%f", &Populacao);
+    printf("Nome do estado = %s \n",nome_do_estado);
 
-printf("Digite a Área: \n");
-scanf("%f", &Área);
+    printf("Código da carta = %s \n",codigo_da_carta);
 
-printf("Digite o PIB: \n");
-scanf("%f", &PIB);
+    printf("Nome da cidade = %s \n",nome_da_cidade);
 
-printf("Digite o número de pontos turísticos: \n");
-scanf("%s", &turísticos);
+    printf("Populacão = %d \n",populacao);
 
-printf("Nome da Cidade: %s \n", Nome);
-printf("Populacao: %f \n", Populacao);
-Printf("Área: %f \n", Área);
-printf("PIB: %f \n",PIB);
-printf("Número de pontos turiísticos: %f \n" ,turísticos);
+    printf("Area em KM = %.1f \n",area_em_km);
 
-return 0;
+    printf("PIB = %.1fmilhões \n",pib);
+
+    printf("Numero de pontos turísticos = %d \n", numero_de_pontos_turisticos);
+
+    //inicio da criação de cartas(scanf).
+
+    printf("------+ Agora é a sua vez! +-----\n");
+
+    printf("DIGITE O NOME DO SEU ESTADO:");
+    
+    scanf("%s", &nome_do_estado);
+
+    printf("seu estado é: %s\n", nome_do_estado);
+
+    printf("DIGITE O CODIGO DA CARTA:");
+    printf("(exemplo:A01,B07,W92)\n");
+
+    scanf("%s", &codigo_da_carta);
+
+    printf("codigo da sua carta:%s\n", codigo_da_carta);
+
+    printf("DIGITE O NOME DA CIDADE:");
+
+    scanf("%s", &nome_da_cidade);
+
+    printf("o nome da cidade:%s\n", nome_da_cidade);
+
+    printf("DIGITE A POPULAÇÃO DA CIDADE:");
+
+    scanf("%d", &populacao);
+
+    printf("a população:%d\n", populacao);
+
+    printf("DIGITE A AREA EM 'KM':");
+
+    scanf("%f", &area_em_km);
+
+    printf("sua area em KM:%f\n", area_em_km);
+
+    printf("digite o PIB da cidade:");
+
+    scanf("%f", &pib);
+
+    printf("o PIB da sua cidade:%f\n",pib);
+
+    printf("digite o numero TOTAL de pontos turisticos da cidade:");
+
+    scanf("%d",&numero_de_pontos_turisticos);
+
+    printf("o numero total de pontos turísticos:%d\n",numero_de_pontos_turisticos);
+
+    printf("-----+ SUA CARTA COMPLETA +-----\n");
+
+    // vms ver
+    printf("Nome do estado = %s \n",nome_do_estado);
+
+    printf("Código da carta = %s \n",codigo_da_carta);
+
+    printf("Nome da cidade = %s \n",nome_da_cidade);
+
+    printf("Populacão = %d \n",populacao);
+
+    printf("Area em KM = %.1f \n",area_em_km);
+
+    printf("PIB = %.1fmilhões \n",pib);
+
+    printf("Numero de pontos turísticos = %d \n", numero_de_pontos_turisticos);
+
+    
+
+
+    return 0;
 }
